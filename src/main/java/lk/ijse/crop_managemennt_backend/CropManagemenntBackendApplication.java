53143668ec6c5ded808bc975c95460f1803e7ac7
danukarangith <danukarangith456@gmail.com>
2024-11-08 13:34:38 +0530

    updated

--- src/main/java/lk/ijse/crop_managemennt_backend/CropManagemenntBackendApplication.java
+++ src/main/java/lk/ijse/crop_managemennt_backend/CropManagemenntBackendApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CropManagemenntBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CropManagemenntBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CropManagemenntBackendApplication.class, args);
+        System.out.println("hello");
+    }
+
 
 }
