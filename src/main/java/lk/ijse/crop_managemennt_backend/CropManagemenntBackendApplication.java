f38e8df38d9b7b03f21cb7aed04eb40a06a2d4c5
danukarangith <danukarangith456@gmail.com>
2024-11-08 10:19:50 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/crop_managemennt_backend/CropManagemenntBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_managemennt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagemenntBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropManagemenntBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/crop_managemennt_backend/CropManagemenntBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_managemennt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropManagemenntBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
