cb64b355ea34e03002474740ead88ac3f9541f2d
danukarangith <danukarangith456@gmail.com>
2024-12-05 03:02:16 +0530

    validation add

--- src/main/java/lk/ijse/crop_managemennt_backend/dto/CropDTO.java
+++ src/main/java/lk/ijse/crop_managemennt_backend/dto/CropDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.crop_managemennt_backend.dto;
 
+import jakarta.validation.constraints.*;
 import lk.ijse.crop_managemennt_backend.customObj.CropResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,11 +11,26 @@ import lombok.NoArgsConstructor;
 @Data
 
 public class CropDTO implements SuperDTO, CropResponse {
+    @NotBlank(message = "Crop code cannot be blank")
     private String cropCode;
+
+    @NotBlank(message = "Common name cannot be blank")
+    @Size(min = 2, max = 100, message = "Common name must be between 2 and 100 characters")
     private String cropCommonName;
+
+    @NotBlank(message = "Scientific name cannot be blank")
+    @Size(min = 2, max = 100, message = "Scientific name must be between 2 and 100 characters")
     private String cropScientificName;
+
+
     private String cropImage;
+
+    @NotBlank(message = "Category cannot be blank")
     private String category;
+
+    @NotBlank(message = "Crop season cannot be blank")
     private String cropSeason;
+
+    @NotBlank(message = "Field code cannot be blank")
     private String fieldCode;
 }
