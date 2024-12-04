d27f2dec33543d1322b0d402f8066641c5112714
danukarangith <danukarangith456@gmail.com>
2024-12-05 03:02:26 +0530

    validation add

--- src/main/java/lk/ijse/crop_managemennt_backend/dto/EquipmentDTO.java
+++ src/main/java/lk/ijse/crop_managemennt_backend/dto/EquipmentDTO.java
@@ -1,5 +1,7 @@
 package lk.ijse.crop_managemennt_backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lk.ijse.crop_managemennt_backend.customObj.EquipmentResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +12,22 @@ import lombok.NoArgsConstructor;
 @Data
 
 public class EquipmentDTO implements SuperDTO, EquipmentResponse {
+    @NotBlank(message = "Equipment ID cannot be blank")
     private String equipmentId;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
+
+    @NotBlank(message = "Type cannot be blank")
     private String type;
+
+    @NotBlank(message = "Status cannot be blank")
     private String status;
+
+    @NotBlank(message = "Staff ID cannot be blank")
     private String staffId;
+
+    @NotBlank(message = "Field code cannot be blank")
     private String fieldCode;
 }
